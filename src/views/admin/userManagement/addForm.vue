391f69f3732acb227f6676d3a6a3f34902690456
YiLiuHFUT <389041237@qq.com>
2023-03-20 14:56:42 +0800

    Update addForm.vue

--- src/views/admin/userManagement/addForm.vue
+++ src/views/admin/userManagement/addForm.vue
@@ -1,195 +1,281 @@
 <template>
-  <Modal v-model="modalVisible" :title="AppliForm.id ? '编辑' : '新增'" width="550px"
-    class-name="vertical-center-modal" :mask-closable="false" @on-cancel="modalCancel"
-    @on-visible-change="visibleChange">
-    <div class="content">
-      <Form ref="formRef" class="cdp-form" :rules="ruleValidate" :model="AppliForm" :label-width="120">
-        <FormItem label="用户名" prop="userName">
-          <Input class="cdp-input" v-model="AppliForm.userName"></Input>
-        </FormItem>
-        <FormItem label="密码" prop="password">
-          <Input class="cdp-input" v-model="AppliForm.password" type="password"></Input>
-        </FormItem>
-        <FormItem label="昵称" prop="neakName">
-          <Input class="cdp-input" v-model="AppliForm.neakName"></Input>
-        </FormItem>
-        <FormItem label="手机号" prop="phone">
-          <Input class="cdp-input" v-model="AppliForm.phone"></Input>
-        </FormItem>
-        <FormItem label="所属角色" prop="jobName">
-          <Select class="cdp-input" v-model="AppliForm.jobName">
-            <Option value="系统管理员">系统管理员</Option>
-            <Option value="客户">客户</Option>
-            <Option value="卖家">卖家</Option>
-          </Select>
-        </FormItem>
-        <FormItem label="状态" prop="status">
-          <i-switch v-model="AppliForm.status" true-value="是" false-value="否" true-color="#13ce66" false-color="#ff4949">
-            <span slot="open">是</span>
-            <span slot="close">否</span>
-          </i-switch>
-        </FormItem>
-      </Form>
-    </div>
-    <template #footer>
-      <div style="text-align: center">
-        <Button @click="modalCancel">取消</Button>
-        <Button type="primary" :loading="loading" @click="modalOK">确定</Button>
+  <div class="login">
+    <div class="main">
+      <div class="title">
+        <div class="title-wrap">
+          <!-- <img src="@/assets/header/znlogo.jpg" alt="" class="logo"> -->
+          <span class="company">数据交易平台后台（注册）</span>
+        </div>
       </div>
-    </template>
-  </Modal>
+      <div class="form">
+        <Form ref="formRef" :model="AppliForm" :label-width="80" :rules="loginRules">
+        <FormItem prop="username" label="用户名">
+          <Input v-model="AppliForm.username" type="text" auto-complete="off" @keyup.enter.native="registerFunc">
+            <template #prefix>
+                <Icon type="ios-person-outline" />
+            </template>
+          </Input>
+        </FormItem>
+          <FormItem prop="shopname" label="店铺名">
+            <Input v-model="AppliForm.shopname" type="text" auto-complete="off" @keyup.enter.native="registerFunc">
+              <template #prefix>
+                <Icon type="ios-home-outline" />
+              </template>
+            </Input>
+          </FormItem>
+          <FormItem prop="password" label="密码">
+            <Input v-model="AppliForm.password" type="password" password auto-complete="off" @keyup.enter.native="registerFunc">
+              <template #prefix>
+                <Icon type="ios-lock-outline" />
+              </template>
+            </Input>
+          </FormItem>
+          <FormItem prop="repassword" label="确认密码">
+            <Input v-model="AppliForm.repassword" type="password" password auto-complete="off"  @keyup.enter.native="registerFunc">
+              <template #prefix>
+                <Icon type="ios-lock-outline" />
+              </template>
+            </Input>
+          </FormItem>
+          <FormItem prop="phone" label="手机号">
+            <Input v-model="AppliForm.phone" type="tel" auto-complete="off" placeholder="手机号" @keyup.enter.native="registerFunc">
+              <template #prefix>
+                <Icon type="ios-call-outline" />
+              </template>
+            </Input>
+          </FormItem>
+          <FormItem prop="idcard" label="身份证号">
+            <Input v-model="AppliForm.idcard" auto-complete="off" placeholder="身份证号" @keyup.enter.native="registerFunc">
+              <template #prefix>
+                <Icon type="ios-card-outline" />
+              </template>
+            </Input>
+          </FormItem>
+          <FormItem prop="email" label="邮箱">
+            <Input v-model="AppliForm.email" type="email" auto-complete="off" placeholder="邮箱" @keyup.enter.native="registerFunc">
+              <template #prefix>
+                <Icon type="ios-mail-outline" />
+              </template>
+            </Input>
+          </FormItem>
+          <Row :gutter="20">
+            <Col span="12">
+              <Button :loading="loading" size="medium" type="primary" style="width:100%;" @click.native.prevent="registerFunc">
+                <span v-if="!loading">注 册</span>
+                <span v-else>注 册 中...</span>
+              </Button>
+            </Col>
+            <Col span="12">
+              <Button size="medium" type="warning" style="width:100%;" @click.native.prevent="loginUp">
+                去登录
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import { useForm, useState, useUserInfo } from '@/hook/index'
+<script setup>
+import { loginUser } from '@/api/index';
+import { useForm, useState, useRouter} from "@/hook/index.js";
 import { Message } from "view-ui-plus";
-import { toRef, computed } from "vue";
-// import { cutoverLevelLimitAdd, cutoverLevelLimitEdit } from '@/api/index';
-import moment from 'moment';
 
-const ruleValidate = {
-  userName: [
-    { required: true, message: '用户名不能为空'}
-  ],
-  password: [
-    { required: true, message: '密码不能为空'}
-  ],
-  neakName: [
-    { required: true, message: '昵称不能为空'}
-  ],
-  phone: [
-    { required: true, message: '手机号不能为空'}
-  ],
-  jobName: [
-    { required: true, message: '所属角色不能为空'}
-  ],
-  status: [
-    { required: true, message: '状态不能为空'}
-  ]
+let validatePass2 = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== this.loginForm.password) {
+    callback(new Error('两次输入密码不一致!'))
+  } else {
+    callback()
+  }
+}
+let phonePass = (rule, value, callback) => {
+  let reg = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
+  if (value === '') {
+    callback(new Error('请输入手机号'))
+  } else if (!reg.test(value)) {
+    callback(new Error('手机号格式不正确'))
+  } else {
+    callback()
+  }
+}
+let idcardPass = (rule, value, callback) => {
+  let IDRe18 = /^([1-6][1-9]|50)\d{4}(18|19|20)\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
+  let IDre15 =  /^([1-6][1-9]|50)\d{4}\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}$/
+  if (value === '') {
+    callback(new Error('请输入身份证号'))
+  } else if (!IDRe18.test(value) && !IDre15.test(value)) {
+    callback(new Error('身份证号格式不正确'))
+  } else {
+    callback()
+  }
+}
+let emailPass = (rule, value, callback) => {
+  let emailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+  if (value === '') {
+    callback()
+  } else if (!emailReg.test(value)) {
+    callback(new Error('邮箱格式不正确'))
+  } else {
+    callback()
+  }
 }
 
-export default {
-  props: {
-    detailInfo: {
-      type: Object,
-      default: () => {}
-    },
-    visible: {
-      type: Boolean,
-      default: false
-    }
-  },
-  components: {
-  },
-  setup(props, { emit }) {
+const loginRules = {
+  username: [{ required: true, trigger: 'blur', message: '用户名不能为空' }],
+  shopname: [{ required: true, trigger: 'blur', message: '店铺名不能为空' }],
+  password: [{ required: true, trigger: 'blur', message: '密码不能为空' }],
+  repassword: [{ required: true, validator: validatePass2, trigger: 'blur' }],
+  phone: [{ required: true, validator: phonePass, trigger: 'blur' }],
+  idcard: [{ required: true, validator: idcardPass, trigger: 'blur' }],
+  email: [{ validator: emailPass, trigger: 'blur' }]
+}
 
-    const modalVisible = toRef(props, 'visible');
-    const { userInfo } = useUserInfo()
+// 表单
+const form = {
+  username: "",
+  shopname: "",
+  password: "",
+  repassword: "",
+  phone: "",
+  idcard: "",
+  email: "",
+}
+const [ formRef, AppliForm, resetForm, validateForm ] = useForm(form);
 
-    // 表单
-    const form = {
-      id: "",
-      userName: "",
-      password: "",
-      neakName: "",
-      phone: "",
-      jobName: "",
-      status: "是",
-      createUserId: "",
-      createUserName: ""
-    }
-    const [ formRef, AppliForm, resetForm, validateForm ] = useForm(form)
+const [loading, setLoading] = useState(false);
 
-    const modalCancel = () => { // 点击取消
-      resetForm(true);
-      emit('closeModal')
-    }
+const { router } = useRouter()
 
-    const [loading, setLoading] = useState()
-    const modalOK = async() => { //点击确定
-      let boolean = await validateForm()
-      if(!boolean) {
-        return Message.error("请填写完整");
-      }
-      let params = {
-        userName: AppliForm.userName,
-        password: AppliForm.password,
-        neakName: AppliForm.neakName,
-        jobName: AppliForm.jobName,
-        phone: AppliForm.phone,
-        status: AppliForm.status,
-        id: AppliForm.id,
-        createUserId: AppliForm.createUserId,
-        createUserName: AppliForm.createUserName
-      }
-      // setLoading(true);
-      // let func = !AppliForm.id ? cutoverLevelLimitAdd : cutoverLevelLimitEdit
-      // func(params).then(res => {
-      //   setLoading(false);
-      //   if(res.data.code === 200) {
-      //     modalCancel();
-      //     emit('updateList');
-      //     Message.success(res.data.message);
-      //   }else{
-      //     Message.error(res.data.message);
-      //   }
-      // }, err => {
-      //   setLoading(false);
-      // })
+// 验证码
+const [codeUrl, setCodeUrl] = useState("");
+const getCode = () => {
+  
+}
 
-      modalCancel();
-    }
-
-    const visibleChange = (value) => {
-      if(value) {   // 打开模态框执行
-        AppliForm.id = props.detailInfo.id ? props.detailInfo.id : "";
-        AppliForm.userName = props.detailInfo.userName ? props.detailInfo.userName : "";
-        AppliForm.password = props.detailInfo.password ? props.detailInfo.password : "";
-        AppliForm.neakName = props.detailInfo.neakName ? props.detailInfo.neakName : "";
-        AppliForm.phone = props.detailInfo.phone ? props.detailInfo.phone : "";
-        AppliForm.jobName = props.detailInfo.jobName ? props.detailInfo.jobName : "";
-        AppliForm.status = props.detailInfo.status ? props.detailInfo.status : "是";
-        // AppliForm.createUserId = props.detailInfo.createUserId ? props.detailInfo.createUserId : userInfo.userId;
-        // AppliForm.createUserName = props.detailInfo.createUserName ? props.detailInfo.createUserName : userInfo.userName;
-      }
-    }
-
-    return{
-      formRef,
-      ruleValidate,
-      AppliForm,
-      modalCancel,
-      modalOK,
-      loading,
-      visibleChange,
-      modalVisible,
-    }
+const registerFunc = async() => {
+  let boolean = await validateForm()
+  if(!boolean) {
+    return Message.error("请填写完整注册信息");
   }
+  let params = {
+    username: AppliForm.username,
+    shopname: AppliForm.shopname,
+    password: AppliForm.password,
+    repassword: AppliForm.repassword,
+    phone: AppliForm.phone,
+    idcard: AppliForm.idcard,
+    email: AppliForm.email,
+  }
+  // setLoading(true);
+  // loginUser(params).then(res => {
+  //   setLoading(false);
+  //   if(res.data.code === 200) {
+  //     Message.success("登录成功")
+  //     window.sessionStorage.setItem("TOKEN", "znwy")
+  //     router.push({
+  //       path: '/admin/newsManagement'
+  //     })
+  //   }else {
+  //     Message.error(res.data.msg)
+  //   }
+  // }, err => {
+  //   setLoading(false);
+  // })
+  router.push({
+    path: '/admin/login'
+  })
+}
+
+const loginUp = () => {
+  router.push({
+    path: '/admin/login'
+  })
 }
 </script>
 
-<style scoped lang="less">
-:deep(.vertical-center-modal){
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .ivu-modal{
-    top: 0;
-    .ivu-modal-content{
-      .ivu-modal-header{
-        padding: 10px;
-      }
-      .ivu-modal-body{
-        padding: 10px;
-      }
-      .ivu-modal-footer{
-        padding: 10px;
-      }
-    }
-  }
-}
+<style lang='stylus' scoped>
+@import "view-ui-plus/dist/styles/viewuiplus.css";
 
-.cdp-input{
-  width: 100%;
-}
+.login
+  position relative
+  width 100vw
+  height 100vh
+  color #000
+  background linear-gradient(rgba(51, 51, 51, 1) 50%, rgba(239, 239, 239, 1) 50%)
+  .main
+    position absolute
+    width 562px
+    height 518px
+    margin auto
+    top 0
+    bottom 0
+    left 0
+    right 0
+    .title
+      width 100%
+      height 58px
+      .title-wrap
+        width 63%
+        margin 0 auto
+        text-align center
+        .logo
+          padding 10px 20px
+          width 78px
+          height 58px
+          box-sizing border-box
+          background-size contain
+        .company
+          height 58px
+          line-height 58px
+          font-family '微软雅黑 Bold', '微软雅黑 Regular', '微软雅黑'
+          font-weight 700
+          font-style normal
+          font-size 28px
+          color #FFFFFF
+          vertical-align top
+    .form
+      position relative
+      height 512px
+      background #FFF
+      padding 50px 75px
+      .user, .pwd
+        margin auto
+        &:after
+          content ''
+          display table
+          clear both
+        span
+          display inline-block
+          float left
+          line-height 39px
+          font-family '微软雅黑'
+          font-weight 400
+          font-style normal
+          font-size 16px
+          color #666666
+        .i-login
+          width 317px
+          height 39px
+          float right
+          line-height 39px
+      .pwd
+        margin-top 25px
+      .btn
+        width 197px
+        height 48px
+        margin-top 45px
+        margin-left 155px
+        background-color rgba(51, 51, 51, 1)
+        color #FFF
+        font-family '微软雅黑'
+        font-weight 400
+        font-style normal
+        font-size 16px
+      .forget-pwd
+        position absolute
+        bottom 35px
+        right 45px
 </style>
